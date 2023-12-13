9bb2e3a69d11a5038b80e508bf157f93d19ca3fa
F1eshka <144143888+F1eshka@users.noreply.github.com>
2023-12-14 02:08:33 +0300

    Add files via upload

---
+++ HW 31,12.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+class Counter {
+    int now = 0;
+    int min = 0;
+    int max = 9999;
+    int plus = 1;
+
+public:
+    void SetNow(int n) {
+        now = n;
+    }
+    int GetNow() {
+        return now;
+    }
+    void SetMin(int mi) {
+        min = mi;
+    }
+    int GetMin() {
+        return min;
+    }
+    void SetMax(int ma) {
+        max = ma;
+    }
+    int GetMax() {
+        return max;
+    }
+    void SetPlus(int p) {
+        plus = p;
+    }
+    int GetPlus() {
+        return plus;
+    }
+
+    void Function()
+    {
+        while (now <= max) {
+
+            if (now == max) {
+                now = min;
+                Sleep(5000);
+            }
+            now += plus;
+            cout << now << "\n";
+        }
+    }
+};
+
+int main() {
+    Counter c;
+    c.Function();
+}
